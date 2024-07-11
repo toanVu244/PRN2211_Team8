e2535fb9c10f419be5b7e657d66c614e5075c4c8
toanVu244 <vutritoan244@gmail.com>
2024-07-11 12:24:54 +0700

    test commit

---
+++ JPOS.Controller/Pages/Dashboard/User/Delete.cshtml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using JPOS.Model.Entities;
+
+namespace JPOS.Controller.Pages.Dashboard.User
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly JPOS.Model.Entities.JPOS_ProjectContext _context;
+
+        public DeleteModel(JPOS.Model.Entities.JPOS_ProjectContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+      public User User { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(string id)
+        {
+            if (id == null || _context.Users == null)
+            {
+                return NotFound();
+            }
+
+            var user = await _context.Users.FirstOrDefaultAsync(m => m.UserId == id);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+            else 
+            {
+                User = user;
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(string id)
+        {
+            if (id == null || _context.Users == null)
+            {
+                return NotFound();
+            }
+            var user = await _context.Users.FindAsync(id);
+
+            if (user != null)
+            {
+                User = user;
+                _context.Users.Remove(User);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
