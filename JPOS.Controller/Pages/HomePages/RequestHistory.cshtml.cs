e1c0a85d6dec095440d3bf22b2952408c706c365
toanVu244 <vutritoan244@gmail.com>
2024-07-16 01:53:28 +0700

    fix 3 request

--- JPOS.Controller/Pages/HomePages/RequestHistory.cshtml.cs
+++ JPOS.Controller/Pages/HomePages/RequestHistory.cshtml.cs
@@ -11,10 +11,12 @@ namespace JPOS.Controller.Pages.HomePages
     public class RequestHistoryModel : PageModel
     {
         private readonly IRequestService _requestService;
+        private readonly IProductService _productService;
 
-        public RequestHistoryModel(IRequestService requestService)
+        public RequestHistoryModel(IRequestService requestService, IProductService productService)
         {
             _requestService = requestService;
+            _productService = productService;
         }
 
         public List<Request> Requests { get; set; }
@@ -31,19 +33,26 @@ namespace JPOS.Controller.Pages.HomePages
             return Page();
         }
 
-        public IActionResult OnPostCheckout(int id)
+        public async Task<IActionResult> OnPostCheckout(int id)
         {
-            var request = _requestService.GetRequestByIDAsync(id).Result;
+            var request = await _requestService.GetRequestByIDAsync(id);
             if (request != null)
             {
-                TempData["UID"] = request.UserId;
+                /*TempData["UID"] = request.UserId;
                 TempData["Description"] = request.Description;
                 TempData["Status"] = request.Status;
                 TempData["PID"] = request.ProductId.ToString();
                 TempData["Type"] = request.Type.ToString();
-                TempData["TotalMoney"] = "100"; // Replace with actual total money calculation
+                TempData["TotalMoney"] = "100";  Replace with actual total money calculation*/
+                var product = await _productService.GetProductByID(request.ProductId);
+                if(product != null)
+                {
+                    TempData["TotalMoney"] = 10;
+                    TempData["RID"] = request.Id;
+                }
+
             }
-            return RedirectToPage("/HomePages/Checkout");
+            return RedirectToPage("/HomePages/ConfirmCheckout");
         }
     }
 }
